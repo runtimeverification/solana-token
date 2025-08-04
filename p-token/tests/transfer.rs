7924d40eae25705271dd6eca076b7d23b801671c
dkcumming <daniel.cumming@runtimeverification.com>
2025-08-04 19:38:06 +1000

    Duplicated transfer test but with uninit account

--- p-token/tests/transfer.rs
+++ p-token/tests/transfer.rs
@@ -84,3 +84,74 @@ async fn transfer() {
 
     assert!(account.amount == 0);
 }
+
+#[tokio::test]
+async fn transfer_uninit() {
+    let mut context = ProgramTest::new("pinocchio_token_program", TOKEN_PROGRAM_ID, None)
+        .start_with_context()
+        .await;
+
+    // Given a mint account.
+
+    let mint_authority = Keypair::new();
+    let freeze_authority = Pubkey::new_unique();
+
+    let mint = mint::initialize(
+        &mut context,
+        mint_authority.pubkey(),
+        Some(freeze_authority),
+        &TOKEN_PROGRAM_ID,
+    )
+    .await
+    .unwrap();
+
+    // And a token account with 100 tokens.
+
+    let owner = Keypair::new();
+
+    let account =
+        account::initialize(&mut context, &mint, &owner.pubkey(), &TOKEN_PROGRAM_ID).await;
+
+    mint::mint(
+        &mut context,
+        &mint,
+        &account,
+        &mint_authority,
+        100,
+        &TOKEN_PROGRAM_ID,
+    )
+    .await
+    .unwrap();
+
+    // When we transfer the tokens.
+
+    let destination = Pubkey::new_unique();
+
+    let destination_account =
+        account::initialize(&mut context, &mint, &destination, &TOKEN_PROGRAM_ID).await;
+
+    let transfer_ix = spl_token::instruction::transfer(
+        &spl_token::ID,
+        &Pubkey::new_unique(), // <-- Uninitialised Account
+        &destination_account,
+        &owner.pubkey(),
+        &[],
+        100,
+    )
+    .unwrap();
+
+    let tx = Transaction::new_signed_with_payer(
+        &[transfer_ix],
+        Some(&context.payer.pubkey()),
+        &[&context.payer, &owner],
+        context.last_blockhash,
+    );
+    let result = context.banks_client.process_transaction(tx).await;
+    let inner_error = result.err().unwrap().unwrap();
+    
+    // What should happen 
+    // assert_eq!(inner_error, solana_transaction_error::TransactionError::InstructionError(0, solana_instruction::error::InstructionError::UninitializedAccount));
+    
+    // What actually happens
+    assert_eq!(inner_error, solana_transaction_error::TransactionError::InstructionError(0, solana_instruction::error::InstructionError::InvalidAccountData));
+}
