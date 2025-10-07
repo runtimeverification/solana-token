ae008e31f847961128a319070721e73479d4be9b
dkcumming <daniel.cumming@runtimeverification.com>
2025-10-07 16:02:59 +1000

    ERROR: Accessing owner of AccountInfo instead of Account

--- p-token/src/entrypoint-runtime-verification.rs
+++ p-token/src/entrypoint-runtime-verification.rs
@@ -962,7 +962,7 @@ pub fn test_process_burn(accounts: &[AccountInfo; 3], instruction_data: &[u8; 8]
     let src_is_native = get_account(&accounts[0]).is_native();
     let src_mint = get_account(&accounts[0]).mint;
     let src_owned_sys_inc = get_account(&accounts[0]).is_owned_by_system_program_or_incinerator();
-    let src_owner = *accounts[0].owner();
+    let src_owner = get_account(&accounts[0]).owner;
     let old_src_delgate = get_account(&accounts[0]).delegate().cloned();
     let old_src_delgated_amount = get_account(&accounts[0]).delegated_amount();
     let mint_initialised = get_mint(&accounts[1]).is_initialized();
@@ -1537,7 +1537,7 @@ pub fn test_process_burn_checked(accounts: &[AccountInfo; 3], instruction_data: 
     let src_is_native = get_account(&accounts[0]).is_native();
     let src_mint = get_account(&accounts[0]).mint;
     let src_owned_sys_inc = get_account(&accounts[0]).is_owned_by_system_program_or_incinerator();
-    let src_owner = *accounts[0].owner();
+    let src_owner = get_account(&accounts[0]).owner;
     let old_src_delgate = get_account(&accounts[0]).delegate().cloned();
     let old_src_delgated_amount = get_account(&accounts[0]).delegated_amount();
     let mint_initialised = get_mint(&accounts[1]).is_initialized();
