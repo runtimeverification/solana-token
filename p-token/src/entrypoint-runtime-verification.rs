f3b31de31924b13f012b3ae3dc1283bf15a58f13
dkcumming <daniel.cumming@runtimeverification.com>
2025-11-04 13:00:01 +1000

    Reduced calls to `get_account` in `InitializeAccount`
    This DOES NOT reduce the amount of proofs steps compared to if this
    change is not present (both 89207 steps), indicating that our proofs are not getting into the
    `else` brach of the post-condition.

--- p-token/src/entrypoint-runtime-verification.rs
+++ p-token/src/entrypoint-runtime-verification.rs
@@ -731,22 +731,21 @@ pub fn test_process_initialize_account(accounts: &[AccountInfo; 4]) -> ProgramRe
     {
         assert_eq!(result, Err(ProgramError::Custom(2)))
     } else {
+        let new_account_new = get_account(&accounts[0]);
+
         assert!(result.is_ok());
         assert_eq!(
-            get_account(&accounts[0]).account_state().unwrap(),
+            new_account_new.account_state().unwrap(),
             account_state::AccountState::Initialized
         );
-        assert_eq!(get_account(&accounts[0]).mint, *accounts[1].key());
-        assert_eq!(get_account(&accounts[0]).owner, *accounts[2].key());
+        assert_eq!(new_account_new.mint, *accounts[1].key());
+        assert_eq!(new_account_new.owner, *accounts[2].key());
 
         if is_native_mint {
-            assert!(get_account(&accounts[0]).is_native());
+            assert!(new_account_new.is_native());
+            assert_eq!(new_account_new.native_amount().unwrap(), minimum_balance);
             assert_eq!(
-                get_account(&accounts[0]).native_amount().unwrap(),
-                minimum_balance
-            );
-            assert_eq!(
-                get_account(&accounts[0]).amount(),
+                new_account_new.amount(),
                 accounts[0].lamports() - minimum_balance
             );
         }
