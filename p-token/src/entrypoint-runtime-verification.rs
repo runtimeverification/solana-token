88fe7783eeca061fe029b8447a214492c1a461e4
Jost Berthold <jost.berthold@gmail.com>
2025-11-26 09:58:05 +1100

    mint_to_checked: skip execution for overflow case (same as mint_to)

--- p-token/src/entrypoint-runtime-verification.rs
+++ p-token/src/entrypoint-runtime-verification.rs
@@ -3926,6 +3926,14 @@ fn test_process_mint_to_checked(
     let dst_init_state = dst_old.account_state();
     let maybe_multisig_is_initialised = None; // Value set to `None` since authority is an account
 
+    // Do not execute if adding to the account balance would overflow.
+    // shared::mint_to.rs,L68 is based on the assumption that initial_amount <= mint.supply
+    // and therefore cannot overflow because the minting itself would already error out.
+    let amount = unsafe { u64::from_le_bytes(*(instruction_data.as_ptr() as *const [u8; 8])) };
+    if initial_amount.checked_add(amount).is_none() {
+        return Err(ProgramError::ArithmeticOverflow);
+    }
+
     //-Process Instruction-----------------------------------------------------
     let result = process_mint_to_checked(accounts, instruction_data);
 
