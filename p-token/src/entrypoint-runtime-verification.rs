f49d5160f9db7ea1e394967c6b028e41fc95fa95
Jost Berthold <jost.berthold@gmail.com>
2025-11-26 16:07:43 +1100

    formatting according to CI error

--- p-token/src/entrypoint-runtime-verification.rs
+++ p-token/src/entrypoint-runtime-verification.rs
@@ -1339,8 +1339,9 @@ pub fn test_process_mint_to(
     #[cfg(feature = "assumptions")]
     {
         // Do not execute if adding to the account balance would overflow.
-        // shared::mint_to.rs,L68 is based on the assumption that initial_amount <= mint.supply
-        // and therefore cannot overflow because the minting itself would already error out.
+        // shared::mint_to.rs,L68 is based on the assumption that initial_amount <=
+        // mint.supply and therefore cannot overflow because the minting itself
+        // would already error out.
         let amount = unsafe { u64::from_le_bytes(*(instruction_data.as_ptr() as *const [u8; 8])) };
         if initial_amount.checked_add(amount).is_none() {
             return Err(ProgramError::Custom(99));
@@ -3946,8 +3947,9 @@ fn test_process_mint_to_checked(
     #[cfg(feature = "assumptions")]
     {
         // Do not execute if adding to the account balance would overflow.
-        // shared::mint_to.rs,L68 is based on the assumption that initial_amount <= mint.supply
-        // and therefore cannot overflow because the minting itself would already error out.
+        // shared::mint_to.rs,L68 is based on the assumption that initial_amount <=
+        // mint.supply and therefore cannot overflow because the minting itself
+        // would already error out.
         let amount = unsafe { u64::from_le_bytes(*(instruction_data.as_ptr() as *const [u8; 8])) };
         if initial_amount.checked_add(amount).is_none() {
             return Err(ProgramError::Custom(99));
