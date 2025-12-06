43c73f17a1e162abfbc6f18f75dc9fed011c3708
Jost Berthold <jost.berthold@gmail.com>
2025-12-06 19:57:55 +1100

    Make close_account assertion conditional behind solana feature
    Co-authored-by: Daniel Cumming <124537596+dkcumming@users.noreply.github.com>

--- p-token/src/entrypoint-runtime-verification.rs
+++ p-token/src/entrypoint-runtime-verification.rs
@@ -1828,7 +1828,8 @@ pub fn test_process_close_account(accounts: &[AccountInfo; 3]) -> ProgramResult 
             accounts[1].lamports(),
             dst_init_lamports + src_init_lamports
         );
-        // assert_eq!(accounts[0].data_len(), 0); // Solana-RT only
+        #[cfg(any(target_os = "solana", target_arch = "bpf"))]
+        assert_eq!(accounts[0].data_len(), 0); // Solana-RT only
     }
     result
 }
